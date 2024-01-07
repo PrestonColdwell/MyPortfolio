a34be3dfd5e1578909462fe0330582316b57b572
Preston Coldwell <120293789+PrestonColdwell@users.noreply.github.com>
2024-01-07 09:34:31 -0500

    Initial Files
    Uploaded initial files to repository

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <>
+      <h1 className='text-center m-5 text-xl font-bold'>This is my portfolio page!</h1>
+      <div className="text-center">
+        Add shit here about myself...
+      </div>
+    </>
+  )
+}
+
+export default App
